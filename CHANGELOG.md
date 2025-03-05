b332c37126ceb0e4d45884c578ba048debcef203
Koutheir Attouchi <koutheir.attouchi@microdoc.com>
2025-03-05 10:31:35 -0500

    Added SCOLS_DEBUG_* constants.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,12 @@
 
 This project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [0.1.2] - 2025-03-05
+
+### Added
+
+- `SCOLS_DEBUG_*` constants, useful when calling `scols_init_debug()`.
+
 ## [0.1.1] - 2025-03-04
 
 ### Changed
--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,23 @@
 #![cfg(target_os = "linux")]
 #![doc = include_str!("../README.md")]
-#![doc(html_root_url = "https://docs.rs/smartcols-sys/0.1.1")]
+#![doc(html_root_url = "https://docs.rs/smartcols-sys/0.1.2")]
 #![allow(non_camel_case_types)]
 
 #[cfg(test)]
 mod tests;
+
+use std::os::raw::c_uint;
+
+pub const SCOLS_DEBUG_HELP: c_uint = 1 << 0;
+pub const SCOLS_DEBUG_INIT: c_uint = 1 << 1;
+pub const SCOLS_DEBUG_CELL: c_uint = 1 << 2;
+pub const SCOLS_DEBUG_LINE: c_uint = 1 << 3;
+pub const SCOLS_DEBUG_TAB: c_uint = 1 << 4;
+pub const SCOLS_DEBUG_COL: c_uint = 1 << 5;
+pub const SCOLS_DEBUG_BUFF: c_uint = 1 << 6;
+pub const SCOLS_DEBUG_GROUP: c_uint = 1 << 7;
+pub const SCOLS_DEBUG_FLTR: c_uint = 1 << 8;
+pub const SCOLS_DEBUG_FPARAM: c_uint = 1 << 9;
+pub const SCOLS_DEBUG_ALL: c_uint = 0xFFFF;
 
 include!(concat!(env!("OUT_DIR"), "/smartcols-sys.rs"));
